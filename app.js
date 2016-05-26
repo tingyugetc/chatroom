79591e6de7a72b9ecedda4bbf0e63150ad3f246e
fengyuanzemin <fengyuanzemin@gmail.com>
2016-05-26 22:19:02 +0800

    能点击头像进行私聊的初始化

--- app.js
+++ app.js
@@ -19,22 +19,22 @@ const md = new MarkdownIt({
     breaks: true,
     linkify: true
 });
-let users=[];
+let users = [];
 const routes = require('./routes/index');
 // const notFound =require('./routes/404');
 
 const app = express();
 
-const server=app.listen(3000);
-const io=require('socket.io').listen(server);
+const server = app.listen(3000);
+const io = require('socket.io').listen(server);
 // 防范XSS
-const xss = function (a) {
-  return String(a)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
+const xss = function(a) {
+    return String(a)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
 };
 // 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
@@ -59,31 +59,31 @@ app.use('/', routes);
 // 定制404页面
 app.use(function(req, res) {
     res.status(404);
-    res.render('404',{ title: '路径错啦' })
+    res.render('404', { title: '路径错啦' })
 });
 
 app.use(function(req, res) {
     console.error(err.stack);
     res.status(500);
-    res.render('error',{title:'服务器内部错误啦'});
+    res.render('error', { title: '服务器内部错误啦' });
 });
 
 io.on('connection', function(socket) {
 
     socket.on('user connection', function(msg) {
-        msg=xss(msg);
-        let avatar=Math.floor(Math.random()*10+1);
-        console.log(avatar);
+        // 有一个客户端建立连接后，就把该用户加入数组里
+        msg = xss(msg);
+        let avatar = Math.floor(Math.random() * 13 + 1);
         users.push({
             id: socket.id,
             name: msg,
             avatar: avatar
-        })
-        console.log(users);
-        io.sockets.emit('hi', msg + '加入聊天室 当前在线人数 ' + users.length);
+        });
+        io.emit('hi', msg + ' 加入聊天室 当前在线人数 ' + users.length);
     });
 
     socket.on('disconnect', function() {
+        // 断开连接后，就把它从数组里面删掉
         let i = _.findIndex(users, {
             id: socket.id
         });
@@ -92,7 +92,7 @@ io.on('connection', function(socket) {
             _.remove(users, function(u) {
                 return u.id == socket.id;
             })
-            io.sockets.emit('hi', _user.name + '离开了聊天室 当前在线人数' + users.length);
+            io.emit('hi', _user.name + ' 离开了聊天室 当前在线人数' + users.length);
         }
     });
 
@@ -104,9 +104,40 @@ io.on('connection', function(socket) {
             if (i >= 0) {
                 io.emit('chat message', {
                     user: users[i],
-                    msg: md.render(msg)
+                    msg: md.render(msg),
                 });
             }
+        }
+    });
+
+    socket.on('whisper-first',function(msg){
+        // 说明是其他客户端的消息
+        if(msg!==socket.id){
+            let i = _.findIndex(users, {
+                id: msg
+            });
+            if(i>=0){
+                socket.broadcast.to(msg).emit('whisper-second', {
+                    to:msg,
+                    from:socket.id,
+                    user:users[i],
+                 });
+            }
+            
+        }
+    });
+    socket.on('whisper-third',function(msg){
+
+        // 同意进行私聊后，给from的人发送消息，连接开始
+        let i = _.findIndex(users, {
+                id: msg.to
+            });
+        if(i>=0){
+            socket.broadcast.to(msg.from).emit('whisper-forth',{
+                to:msg.to,
+                from:msg.from,
+                user:users[i]
+            });
         }
     });
 
